e4777f4e767e45ffc3c7f2762245c40676714634
Starcatch <timurmishiev@gmail.com>
2020-07-10 01:17:08 -0400

    add edit and update action in the controller and create an edit template form

--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -12,6 +12,10 @@ class DailyLogsController < ApplicationController
     @daily_log = DailyLog.new
   end 
 
+  def edit
+    @daily_log = DailyLog.find(params[:id])
+  end 
+
   def create
     @daily_log = DailyLog.new(params.require(:daily_log).permit(:shift, :notes))
     if  @daily_log.save 
@@ -19,6 +23,16 @@ class DailyLogsController < ApplicationController
     redirect_to daily_log_path(@daily_log) #extracts daily_log's id 
     else
       render 'new'
+    end 
+  end 
+
+  def update
+    @daily_log = DailyLog.find(params[:id])
+    if @daily_log.update(params.require(:daily_log).permit(:shift, :notes))
+      flash[:notice] = "The Daily Log was updated succesfully"
+      redirect_to daily_log_path(@daily_log)
+    else 
+      render 'edit'
     end 
   end 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :daily_logs, only: [:show, :index, :new, :create]
+  resources :daily_logs, only: [:show, :index, :new, :create, :edit, :update]
 
 end
