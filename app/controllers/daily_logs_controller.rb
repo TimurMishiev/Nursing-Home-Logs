b18c5db18b65c21510f8ae30dbb4f32037ab1db5
Starcatch <timurmishiev@gmail.com>
2020-07-09 23:53:13 -0400

    create a form to sumbit a new daily_log

--- app/controllers/daily_logs_controller.rb
+++ app/controllers/daily_logs_controller.rb
@@ -1,14 +1,14 @@
 class DailyLogsController < ApplicationController
 
   def show
-    @daily_logs = DailyLog.find(params[:id])
+    @daily_log = DailyLog.find(params[:id])
   end 
 
   def index
     @daily_logs = DailyLog.all
   end 
 
-  def show
+  def new
   end 
 
   def create
