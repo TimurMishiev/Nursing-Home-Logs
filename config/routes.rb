9157cbda53e44a8ce741e0a6f8bd04c44b88c82b
Starcatch <timurmishiev@gmail.com>
2020-07-10 20:23:11 -0400

    add bootstrap for my app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  
   resources :daily_logs
+  get 'signup', to: 'users#new'
+
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
 end
