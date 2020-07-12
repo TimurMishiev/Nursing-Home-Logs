f98b7fcd224ecfb6e8464b6b473ac6f5f5509191
Starcatch <timurmishiev@gmail.com>
2020-07-12 18:14:46 -0400

    add user authentication and a log out action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,23 @@ class SessionsController < ApplicationController
   end 
 
   def create
+    user = User.find_by(username: params[:session][username].downcase)
+    if user &&  user.authenticate(params[:session][:password])
+      session[user_id] = user.id #allows our user to authonticate once , and then remained signed in for all the requests.
+      flash[:notice] = "Logged in successfully"
+      redirect_to user
+    
+    else
+      flash.now[:alert] = "There was something wrong with your login details"
+      render 'new' #we are displaying a new form over again, and not redirecting.
+
+    end 
   end 
 
   def destroy
+    session [:user_id] = nil
+    flash [:notice] = "Logged out"
+    redirect_to root_path
   end 
 
 end 
