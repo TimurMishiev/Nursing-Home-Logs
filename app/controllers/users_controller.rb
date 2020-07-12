22b7e5f9abcf815c6251afc213c7211ffba635b3
Starcatch <timurmishiev@gmail.com>
2020-07-12 01:51:25 -0400

    add users create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Welcome to Our Home, you have successfully signed up "
+      flash[:notice] = "Welcome to Our Home #{@user.username}, you have successfully signed up "
       redirect_to daily_logs_path
     else
       render 'new'
