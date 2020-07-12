0a6d2f723982d7ea76c55a594b975082d62cf5c2
Starcatch <timurmishiev@gmail.com>
2020-07-12 01:07:48 -0400

    add has_secure_password in user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ApplicationRecord 
   has_many :daily_logs
+  
 
   validates :username, presence: true, 
                       uniqueness: { case_sensitive: false }, 
                       length: {minimum: 3, maximum: 25}
+  has_secure_password
 end 
