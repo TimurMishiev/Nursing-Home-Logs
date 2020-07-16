0f214b04731d8a170b29075af0da719d1050e97d
Starcatch <timurmishiev@gmail.com>
2020-07-15 23:49:18 -0400

    individual_vitals_logs_path

--- app/controllers/individual_vitals_logs_controller.rb
+++ app/controllers/individual_vitals_logs_controller.rb
@@ -2,32 +2,28 @@ class IndividualVitalsLogsController < ApplicationController
   
   def new
     @individual = Individual.find_by_id(params[:individual_id])
-    @vitals_log = @individual.individual_vitals_logs.build
-  end
-
-  def create
-    @vitals_log = IndividualVitalsLog.new(vitals_log_params)
-    if @vitals_log.save
-    redirect_to individual_vitals_log_path(@vitals_log)
-    else
-        render :new
-    end 
+    @individual_vitals_log = @individual.individual_vitals_logs.build
   end 
 
- 
+
+  def create
+    @individual_vitals_log = IndividualVitalsLog.new(individual_vitals_log_params)
+    redirect_to individual_vitals_logs_path(@individual_vitals_log)
+  end 
 
   def show
-    @vitals_log = IndividualVitalsLog.find_by_id(params[:id])
- 
-  end
+
+  
+  end 
 
   def index
- 
-  end
 
-  private 
+  end 
 
-  def vitals_log_params
+
+  private
+
+  def individual_vitals_log_params
     params.require(:individual_vitals_log).permit(:individual_id, :temperature, :bloodpressure, :pulse)
   end 
   
