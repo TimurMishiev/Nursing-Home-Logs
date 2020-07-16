24005bf4be76c005d0e995589f8307cce36fa465
Starcatch <timurmishiev@gmail.com>
2020-07-15 21:56:24 -0400

    create new page for InvididualVitalsLogs

--- app/controllers/individual_vitals_logs_controller.rb
+++ app/controllers/individual_vitals_logs_controller.rb
@@ -1,15 +1,34 @@
 class IndividualVitalsLogsController < ApplicationController
   
-  def index
-    @individual = Individual.find_by_id(params[:individual_id])
-    @vitals_log = @individual.build_vitals_log
-  end
-
-
-  
   def new
-    @individual_vitals_log = IndividualVitalsLog.new
+    @individual = Individual.find_by_id(params[:individual_id])
+    @vitals_log = @individual.individual_vitals_logs.build
   end
 
+  def create
+    @vitals_log = IndividualVitalsLog.new(vitals_log_params)
+    if @vitals_log.save
+    redirect_to individual_vitals_log_path(@vitals_log)
+    else
+        render :new
+    end 
+  end 
+
+ 
+
+  def show
+    @vitals_log = IndividualVitalsLog.find_by_id(params[:id])
+ 
+  end
+
+  def index
+ 
+  end
+
+  private 
+
+  def vitals_log_params
+    params.require(:individual_vitals_log).permit(:individual_id, :temperature, :bloodpressure, :pulse)
+  end 
   
 end
--- app/models/individual_vitals_log.rb
+++ app/models/individual_vitals_log.rb
@@ -1,3 +1,8 @@
 class IndividualVitalsLog < ApplicationRecord
   belongs_to :individual
+  belongs_to :user
+
+  validates :temperature, presence: true
+  validates :bloodpressure, presence: true
+  validates :pulse, presence: true
 end
